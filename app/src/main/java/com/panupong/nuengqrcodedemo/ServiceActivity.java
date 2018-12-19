e6ab219bf3325a813807a729db3b7e3a5dfbbb4a
nueng <kalumkuyyy@gmail.com>
2018-12-19 15:10:38 +0700

    Drawer Menu Success

--- app/src/main/java/com/panupong/nuengqrcodedemo/ServiceActivity.java
+++ app/src/main/java/com/panupong/nuengqrcodedemo/ServiceActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class ServiceActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
     }
-}
+
+
+}//Main
