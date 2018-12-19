fffb97fa19df5adf6b60b52ae1e227662ab3ba0a
nueng <kalumkuyyy@gmail.com>
2018-12-19 15:45:18 +0700

    Log Out Success

--- app/src/main/java/com/panupong/nuengqrcodedemo/ServiceActivity.java
+++ app/src/main/java/com/panupong/nuengqrcodedemo/ServiceActivity.java
@@ -1,9 +1,21 @@
 package com.panupong.nuengqrcodedemo;
 
+import android.Manifest;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ServiceActivity extends AppCompatActivity {
+
+//    Explicit
+    private String nameString;
+
 
 
 
@@ -11,7 +23,48 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Find NameLogin
+        findNameLogin();
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }//Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemLogOut) {
+
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.signOut();
+            startActivity(new Intent(ServiceActivity.this,MainActivity.class));
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void findNameLogin() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        nameString = firebaseUser.getDisplayName();
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setSubtitle(nameString);
     }
 
 
-}//Main
+}//Main Class
