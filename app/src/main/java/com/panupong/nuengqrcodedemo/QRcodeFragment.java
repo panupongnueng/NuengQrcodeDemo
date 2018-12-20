7edb8ccc803455a1cb4047afc5f6496d5c64531a
nueng <kalumkuyyy@gmail.com>
2018-12-20 15:59:21 +0700

    Finish 20Dec 2018

--- app/src/main/java/com/panupong/nuengqrcodedemo/QRcodeFragment.java
+++ app/src/main/java/com/panupong/nuengqrcodedemo/QRcodeFragment.java
@@ -10,6 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -52,6 +57,45 @@ public class QRcodeFragment extends Fragment implements ZXingScannerView.ResultH
         if (!qrString.isEmpty()) {
             //Read able
             Log.d("20decV3","qrString ==> "+qrString);
+
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = firebaseDatabase.getReference()
+                    .child("Product").child(qrString);
+
+
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    ProductModel productModel = dataSnapshot.getValue(ProductModel.class);
+
+                    String nameString = productModel.getNameProduct();
+                    String descripString = productModel.getDescription();
+                    String pictureString = productModel.getPicture();
+                    String amountString = Integer.toString(productModel.getAmount());
+                    String receiveString = productModel.getReceive();
+
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.contentServiceFragment,DetailFragment.detailInstance(
+                                    nameString,
+                                    descripString,
+                                    pictureString,
+                                    receiveString,
+                                    amountString
+                            )).commit();
+
+                    Log.d("20decV3","NameProduct ==> "+nameString);
+
+
+                }
+
+
+
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
 
 
 
